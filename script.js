6d7786f723a9c48c6b1879ec0b3c743d1518209b
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2024-01-21 02:13:20 -0300

    retomando a estilização do projeto, modificando estilos antigos.

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 
+// const images = ["./assets/classic.png", "./assets/light.png",
+//  "./assets/zero.png"]
 
+// const textsArray = ["COCA COLA CLASSICA", "COCA COLA LIGHT", "COCA COLA ZERO"]
 
+// let currentIndex = 0
+// const intervalTime = 3000
 
+// const image = document.getElementsById('image')
+// const imageText =document.getElementsByClassName('image-text')
+
+// function changeImage() {
+//     image.src = image[currentIndex]
+//     imageText.textContent = textsArray[currentIndex];
+//     currentIndex = (currentIndex + 1) %
+// images.length;
+
+// }
+
+// setInterval(changeImage(), intervalTime);
+
+let time = 3000,
+        currentImageIndex = 0,
+        image = document.querySelectorAll('section img'),
+
+    max = image.length;
+
+    function nextImage() {
+    }
+
+    function start() {
+      setInterval(() => {
+        image[currentImageIndex].classList.remove("personagens")
+
+        currentImageIndex ++
+
+        if(currentImageIndex >= max) {
+            currentImageIndex = 0
+        }
+
+        image[currentImageIndex].classList.add("personagens")
+      }, time);
+    }
+
+    window.addEventListener("load", start)
