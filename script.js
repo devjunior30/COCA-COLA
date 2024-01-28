1b1a135d7ab2a32c7ec634baa8b8143b20a043f1
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2024-01-27 21:09:45 -0300

    desativação do script e estilização da pagina

--- script.js
+++ script.js
@@ -20,27 +20,27 @@
 
 // setInterval(changeImage(), intervalTime);
 
-let time = 3000,
-        currentImageIndex = 0,
-        image = document.querySelectorAll('section img'),
+// let time = 3000,
+//         currentImageIndex = 0,
+//         image = document.querySelectorAll('section img'),
 
-    max = image.length;
+//     max = image.length;
 
-    function nextImage() {
-    }
+//     function nextImage() {
+//     }
 
-    function start() {
-      setInterval(() => {
-        image[currentImageIndex].classList.remove("personagens")
+//     function start() {
+//       setInterval(() => {
+//         image[currentImageIndex].classList.remove("personagens")
 
-        currentImageIndex ++
+//         currentImageIndex ++
 
-        if(currentImageIndex >= max) {
-            currentImageIndex = 0
-        }
+//         if(currentImageIndex >= max) {
+//             currentImageIndex = 0
+//         }
 
-        image[currentImageIndex].classList.add("personagens")
-      }, time);
-    }
+//         image[currentImageIndex].classList.add("personagens")
+//       }, time);
+//     }
 
-    window.addEventListener("load", start)
+//     window.addEventListener("load", start)
